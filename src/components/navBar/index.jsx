51c47adc5120de709d930c7b63dee969994a2ae4
Luciano <lucianocisneros22@gmail.com>
2021-07-28 23:48:16 -0300

    Se cambio la estetica, forma y responsive del nav

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,32 +1,37 @@
 import './index.css';
 import { Link } from 'react-router-dom';
 import { CartWidget } from '../CartWidget';
+import { useState } from 'react';
 
 export const NavBar = () => {
+    const [ showItems, setShowItems ] = useState(false);
+
     return (
       <div className="NavBar">
-        
         <nav className="menu">
 
-          <div>
-            <Link to={`/`}><img src="/images/LogoWeb.png" className="menu-logo" alt="Logo" /></Link>
+          <div className='leftSide'>
+
+            <div className='menu-logo-container'>
+              <Link to={`/`}><img src="/images/LogoWeb.png" className="menu-logo" alt="Logo" /></Link>
+            </div>
+            
+            <ul className="menu-items" id={ showItems ? 'hidden' : '' }>
+              <Link to={`/`}><li className='menu-item'>Inicio</li></Link>
+              <Link to={`/productos/sneakers`}><li className='menu-item'>Productos</li></Link>
+              <Link to={`/como-comprar`}><li className='menu-item'>Como Comprar</li></Link>
+              <Link to={`/contacto`}><li className='menu-item'>Contacto</li></Link>
+            </ul>
           </div>
 
-          <ul className="menu-items">
-            <Link to={`/`}><li className='menu-item'>Inicio</li></Link>
-            <Link to={`/productos/sneakers`}><li className='menu-item'>Productos</li></Link>
-            <Link to={`/como-comprar`}><li className='menu-item'>Como Comprar</li></Link>
-            <Link to={`/contacto`}><li className='menu-item'>Contacto</li></Link>
-          </ul>
-
-          <CartWidget />
-
-          <span className="menu-btn">
-                <i className="fa fa-bars"></i>
-          </span>
+          <div className='rigthSide'>
+            <CartWidget />
+            <i onClick={ () => setShowItems(!showItems) }>
+              <img src="/images/menu.png" className='menu-btn' alt="" />
+            </i>
+          </div>
 
         </nav>
-
       </div>
     )
 }
